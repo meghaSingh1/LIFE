076534fdc73512a9b3c7267badacae19cfe349b4
16520511 <42019220+16520511@users.noreply.github.com>
2019-08-26 23:24:36 +0700

    Update settings.py

--- LIFE/settings.py
+++ LIFE/settings.py
@@ -105,16 +105,16 @@ ASGI_APPLICATION = 'LIFE.routing.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'rbjlqbqd',
-#         'USER': 'rbjlqbqd',
-#         'PASSWORD': 'oD1__rpV-9B09jkYfIjnCfROP5I5VC5u',
-#         'HOST': 'john.db.elephantsql.com',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rbjlqbqd',
+        'USER': 'rbjlqbqd',
+        'PASSWORD': 'oD1__rpV-9B09jkYfIjnCfROP5I5VC5u',
+        'HOST': 'john.db.elephantsql.com',
+        'PORT': '5432',
+    }
+}
 
 CHANNEL_LAYERS = {
     "default": {
@@ -126,9 +126,9 @@ CHANNEL_LAYERS = {
     },
 }
 
-DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-}
+# DATABASES = {
+#     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+# }
 
 # DATABASES = {
 #     'default': {
